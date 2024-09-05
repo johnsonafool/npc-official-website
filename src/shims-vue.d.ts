d808d61a840c9ea7419069c605faf5c27297080b
Riley Ho <riley@rileychh.dev>
2024-09-06 00:14:15 +0800

    feat!: rewrite with Nuxt

--- src/shims-vue.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.vue' {
-  import { defineComponent } from 'vue'
-  const component: ReturnType<typeof defineComponent>
-  export default component
-}
-
-declare module 'lodash.debounce';
-declare module 'body-scroll-lock';
-declare module 'vue-progressive-image';
--- src/utils/discordLinkLoader.ts
+++
@@ -1,3 +0,0 @@
-import dcConfig from '@/assets/npcDiscordUrlConfig.json'
-
-export const joinDiscordLink = dcConfig.joinDiscordGroupLink
